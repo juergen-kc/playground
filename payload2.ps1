04ece6dae89130fc48757d6e08a92db5aa9042cc
juergen-kc <me@juergenklaassen.de>
2022-09-21 08:53:09 +0800

    Create payload2.ps1

---
+++ payload2.ps1
@@ -0,0 +1,10 @@
+Set-Location C:\Windows\Temp
+
+#Launch Azure Auth via Device Login
+Start-Process msedge.exe https://microsoft.com/devicelogin
+
+#Trigger AzCopy and present token
+.\AzCopy.exe login
+
+#Download Paylod from Azure Blob 
+.\AzCopy.exe copy "https://blob4autopilot.blob.core.windows.net/aad-blob" "C:\Windows\Temp\autopilot" --recursive
