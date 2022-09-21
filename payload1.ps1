40924d9dd6236543b57a523f0b368a577885e0c5
juergen-kc <me@juergenklaassen.de>
2022-09-21 12:19:21 +0800

    Update payload1.ps1

--- payload1.ps1
+++ payload1.ps1
@@ -1,8 +1,12 @@
+Set-Location C:\Windows\Temp
+
+Start-Transcript -OutputDirectory c:\Windows\Temp -Verbose -ErrorAction Continue
+
 #Download AzCopy
-Invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile C:\WIndows\Temp\AzCopy.zip -UseBasicParsing
+Invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile C:\Windows\Temp\AzCopy.zip -UseBasicParsing
  
 #Expand Archive
-Expand-Archive C:\WIndows\Temp\AzCopy.zip -DestinationPath C:\Windows\Temp\AzCopy\ -Force
+Expand-Archive C:\Windows\Temp\AzCopy.zip -DestinationPath C:\Windows\Temp\AzCopy\ -Force
  
 #Move AzCopy to the destination you want to store it
 Get-ChildItem ./AzCopy/*/azcopy.exe | Move-Item -Destination "C:\Windows\Temp\AzCopy.exe"
