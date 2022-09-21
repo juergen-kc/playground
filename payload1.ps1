b38576b5ad65ce525efb5ab39bc0b8d617a38ae9
juergen-kc <me@juergenklaassen.de>
2022-09-21 08:52:38 +0800

    Create payload1.ps1

---
+++ payload1.ps1
@@ -0,0 +1,12 @@
+#Download AzCopy
+Invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile C:\WIndows\Temp\AzCopy.zip -UseBasicParsing
+ 
+#Expand Archive
+Expand-Archive C:\WIndows\Temp\AzCopy.zip -DestinationPath C:\Windows\Temp\AzCopy\ -Force
+ 
+#Move AzCopy to the destination you want to store it
+Get-ChildItem ./AzCopy/*/azcopy.exe | Move-Item -Destination "C:\Windows\Temp\AzCopy.exe"
+
+#Remove download folder
+Remove-Item -Path C:\Windows\Temp\AzCopy\ -Force -Recurse
+ 
