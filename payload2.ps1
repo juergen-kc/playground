817d273ed67a78401068c559cf89ed075f45b3aa
juergen-kc <me@juergenklaassen.de>
2022-09-22 14:54:10 +0800

    Update payload2.ps1

--- payload2.ps1
+++ payload2.ps1
@@ -2,12 +2,16 @@ Set-Location C:\Windows\Temp
 
 #Launch Azure Auth via Device Login
 Start-Process msedge.exe https://microsoft.com/devicelogin
+Write-Host "Launching Microsoft's DeviceLogin page" -BackgroundColor Black -ForegroundColor Cyan
 
 #Trigger AzCopy and present token
 .\AzCopy.exe login
+Write-Host "Trigger AzCopy to login" -BackgroundColor Black -ForegroundColor Cyan
 
 #Download Paylod from Azure Blob 
 .\AzCopy.exe copy "https://blob4autopilot.blob.core.windows.net/aad-blob" "C:\Windows\Temp\autopilot" --recursive
+Write-Host "Copying required files to this device" -BackgroundColor Black -ForegroundColor Cyan
 
 #Launch AutoPilot-Script 
 powershell.exe C:\Windows\Temp\autopilot\aad-blob\JCAutoPilotKeypass.ps1
+Write-Host "Launching the AutoPilot-Script" -BackgroundColor Black -ForegroundColor Cyan
