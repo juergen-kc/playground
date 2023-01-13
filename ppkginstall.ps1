60dad48bbec950399d58826f7b8b08f9e3764145
Juergen <me@juergenklaassen.de>
2023-01-13 16:41:50 +0800

    Add files via upload

---
+++ ppkginstall.ps1
@@ -0,0 +1,5 @@
+﻿# Download PPKG to Windows\Temp
+Invoke-WebRequest -Uri https://github.com/juergen-kc/playground/blob/9cf1df8a5b97e8e3c801a557d0ed7b517a5dd3c0/MDM-APDU.ppkg -OutFile C:\Windows\Temp\MDM-APDU.ppkg
+
+# Install PPKG
+provtool.exe C:\Windows\Temp\MDM-APDU.ppkg /quiet
