8d51829db682e40c51afdafca7c4c337f2a00f4b
Juergen <12034487+juergen-kc@users.noreply.github.com>
2025-06-25 10:05:47 -0600

    Update create-firstboot-jumpcloud-task.ps1

--- create-firstboot-jumpcloud-task.ps1
+++ create-firstboot-jumpcloud-task.ps1
@@ -1,20 +1,10 @@
-# Set variables
-$TaskName = "InstallJumpCloudAgent"
-$ScriptPath = "C:\Windows\Temp\InstallJumpCloudAgent.ps1"
-
-# Save the install script to disk
+$scriptPath = "C:\Windows\Temp\InstallJumpCloudAgent.ps1"
 $jcScript = @"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/TheJumpCloud/support/master/scripts/windows/InstallWindowsAgent.ps1 -OutFile $ScriptPath
-Start-Process powershell.exe -ArgumentList \"-ExecutionPolicy Bypass -File `"$ScriptPath`" -JumpCloudConnectKey 'jcc_eyJwdWJsaWNLaWNrc3RhcnRVcmwiOiJodHRwczovL2tpY2tzdGFydC5qdW1wY2xvdWQuY29tIiwicHJpdmF0ZUtpY2tzdGFydFVybCI6Imh0dHBzOi8vcHJpdmF0ZS1raWNrc3RhcnQuanVtcGNsb3VkLmNvbSIsImNvbm5lY3RLZXkiOiJiMWU3ZjhhNGEwNjdhNTMxMmI1YzU5YTU0ZTE4ZjVlNmRmOTRkZjE3In0g'\" -Verb RunAs
+Invoke-RestMethod -Method Get -Uri https://raw.githubusercontent.com/TheJumpCloud/support/master/scripts/windows/InstallWindowsAgent.ps1 -OutFile InstallWindowsAgent.ps1
+.\InstallWindowsAgent.ps1 -JumpCloudConnectKey 'jcc_eyJwdWJsaWNLaWNrc3RhcnRVcmwiOiJodHRwczovL2tpY2tzdGFydC5qdW1wY2xvdWQuY29tIiwicHJpdmF0ZUtpY2tzdGFydFVybCI6Imh0dHBzOi8vcHJpdmF0ZS1raWNrc3RhcnQuanVtcGNsb3VkLmNvbSIsImNvbm5lY3RLZXkiOiJiMWU3ZjhhNGEwNjdhNTMxMmI1YzU5YTU0ZTE4ZjVlNmRmOTRkZjE3In0g'
 "@
+Set-Content -Path $scriptPath -Value $jcScript -Encoding UTF8
 
-Set-Content -Path $ScriptPath -Value $jcScript -Force
-
-# Create scheduled task
-$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$ScriptPath`""
-$Trigger = New-ScheduledTaskTrigger -AtStartup
-$Principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Principal
-
-Register-ScheduledTask -TaskName $TaskName -InputObject $Task
+# Register the scheduled task to run once at startup
+schtasks.exe /Create /TN "InstallJumpCloudAgent" /TR "powershell.exe -ExecutionPolicy Bypass -File `"$scriptPath`"" /SC ONSTART /RL HIGHEST /RU "SYSTEM"
