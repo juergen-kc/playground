5907638e12ae359520723a30a1291dac766a27ad
Juergen <12034487+juergen-kc@users.noreply.github.com>
2025-07-02 08:13:35 -0600

    Delete create-firstboot-jumpcloud-task.ps1

--- create-firstboot-jumpcloud-task.ps1
+++
@@ -1,9 +0,0 @@
-$scriptPath = "C:\Windows\Temp\InstallJumpCloudAgent.ps1"
-$jcScript = @"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-RestMethod -Method Get -Uri https://raw.githubusercontent.com/TheJumpCloud/support/master/scripts/windows/InstallWindowsAgent.ps1 -OutFile InstallWindowsAgent.ps1
-.\InstallWindowsAgent.ps1 -JumpCloudConnectKey 'jcc_eyJwdWJsaWNLaWNrc3RhcnRVcmwiOiJodHRwczovL2tpY2tzdGFydC5qdW1wY2xvdWQuY29tIiwicHJpdmF0ZUtpY2tzdGFydFVybCI6Imh0dHBzOi8vcHJpdmF0ZS1raWNrc3RhcnQuanVtcGNsb3VkLmNvbSIsImNvbm5lY3RLZXkiOiJiMWU3ZjhhNGEwNjdhNTMxMmI1YzU5YTU0ZTE4ZjVlNmRmOTRkZjE3In0g'
-"@
-Set-Content -Path $scriptPath -Value $jcScript -Encoding UTF8
-
-schtasks.exe /Create /TN "InstallJumpCloudAgent" /TR "powershell.exe -ExecutionPolicy Bypass -File `"$scriptPath`"" /SC ONSTART /RL HIGHEST /RU "SYSTEM"
