c2e35d26f99da10a8968f90e6c7f4c5f67c35786
Juergen <12034487+juergen-kc@users.noreply.github.com>
2025-07-02 08:13:22 -0600

    Delete install-jc-extension.ps1

--- install-jc-extension.ps1
+++
@@ -1,8 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-# Download and run the JumpCloud agent installer
-$TempPath = "$env:TEMP\InstallWindowsAgent.ps1"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/TheJumpCloud/support/master/scripts/windows/InstallWindowsAgent.ps1" -OutFile $TempPath
-
-# Run with your JumpCloud Connect Key
-& $TempPath -JumpCloudConnectKey "jcc_eyJwdWJsaWNLaWNrc3RhcnRVcmwiOiJodHRwczovL2tpY2tzdGFydC5qdW1wY2xvdWQuY29tIiwicHJpdmF0ZUtpY2tzdGFydFVybCI6Imh0dHBzOi8vcHJpdmF0ZS1raWNrc3RhcnQuanVtcGNsb3VkLmNvbSIsImNvbm5lY3RLZXkiOiJiMWU3ZjhhNGEwNjdhNTMxMmI1YzU5YTU0ZTE4ZjVlNmRmOTRkZjE3In0g"
